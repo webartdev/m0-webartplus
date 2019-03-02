a7710b0b11a93aa397bab1eea3c827aadb3c4af1
Elena Pearson <websiteart@hotmail.com>
2019-03-01 16:13:44 -0800

    additions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-
-// const element = <h1>Hello</h1>;
-// console.log(element);
-// // React.createElement
-// ReactDOM.render(element, document.getElementById("root"));
-
 import React from "react";
 import ReactDOM from "react-dom";
 import IndecisionApp from "./components/IndecisionApp";
